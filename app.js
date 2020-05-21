f085c1d889bc0a8e5162c29954680fb4bdbcf303
Josepeliao <josep.lopez.de.recalde.martorell@everis.com>
2020-05-21 09:43:38 +0200

    added docker with nginx

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express();
 
 // Serve all the static files form the root directory
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/src/'));
 
 // Fallback for all request
 app.get('/*', function(req,res) {
     
-res.sendFile(__dirname + '/landing.html');
+res.sendFile(__dirname + '/src/landing.html');
 
 });
 
